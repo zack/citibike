1ebd3fca45514691f699d80f6f8b57106d462cec
Zack Youngren <zack@youngren.io>
2023-09-01 16:59:30 -0400

    Add pretty progress bars to prisma seed function

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,5 @@
 import { PrismaClient } from '@prisma/client'
+import { ProgressBar } from 'ascii-progress';
 import fs from 'fs';
 import { parse } from 'csv-parse';
 
@@ -8,18 +9,38 @@ const PLAYERS_FILE = 'prisma/seeds/players.csv';
 
 const prisma = new PrismaClient()
 
+function randomColor() {
+  return '#'+Math.floor(Math.random()*16777215).toString(16);
+}
+
+function createProgressBarSchema(title: string) {
+  const paddedTitle = `${title}`.padEnd(7, ' ');
+  return(
+    `[${paddedTitle}].bold:bar.gradient(${randomColor()},${randomColor()})[:current/:total].bold`
+  );
+}
+
+
 // All of the data for the seeds comes from the first facts party. Please take
 // up any quetsions of voracity with the original players. Thanks, players!
 
 async function main() {
   console.log('Clearing database...');
-  // Clears everything from the database, as all relevant records necessarily
-  // cascade from the player model
+  // It isn't usually necessary to delete all of the models independently, but
+  // sometimes if things get in a weird state while testing it might be. The
+  // order here is important too, as it's in reverse-dependent order.
+  await prisma.guess.deleteMany({});
+  await prisma.fact.deleteMany({});
   await prisma.player.deleteMany({});
 
-  console.log('Seeding players...');
+  console.log('Seeding database...');
   fs.readFile(PLAYERS_FILE, (err, fileData) => {
     parse(fileData, { columns: true, trim: true }, (err, rows) => {
+      const progressBar = new ProgressBar({
+        schema: createProgressBarSchema('Players'),
+        total: rows.length,
+      });
+
       rows.forEach(async ({id, name} : { id: string, name: string }) => {
         await prisma.player.create({
           data: {
@@ -27,14 +48,19 @@ async function main() {
             name,
           },
         });
+
+        progressBar.tick();
       });
     });
   });
 
-  console.log('Seeding facts...');
   fs.readFile(FACTS_FILE, (err, fileData) => {
     parse(fileData, { columns: true, trim: true }, (err, rows) => {
-      if (err) { console.log(err); }
+      const progressBar = new ProgressBar({
+        schema: createProgressBarSchema('Facts'),
+        total: rows.length,
+      });
+
       rows.forEach(async ({id, player_id, content, answer} : { id: string, player_id: string, content: string, answer: string }) => {
         await prisma.fact.create({
           data: {
@@ -44,14 +70,19 @@ async function main() {
             content,
           },
         });
+
+        progressBar.tick();
       });
     });
   });
 
-  console.log('Seeding guesses...');
   fs.readFile(GUESSES_FILE, (err, fileData) => {
     parse(fileData, { columns: true, trim: true }, (err, rows) => {
-      if (err) { console.log(err); }
+      const progressBar = new ProgressBar({
+        schema: createProgressBarSchema('Guesses'),
+        total: rows.length,
+      });
+
       rows.forEach(async ({player_id, fact_id, guess} : { player_id: string, fact_id: string, guess: string }) => {
         await prisma.guess.create({
           data: {
@@ -60,6 +91,8 @@ async function main() {
             guess: guess === '1' ? true : false,
           },
         });
+
+        progressBar.tick();
       });
     });
   });
