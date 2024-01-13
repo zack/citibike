3b1240f64305451f45836f1e22253bb3ecc0c88c
Zack Youngren <zack@youngren.io>
2024-01-12 19:48:20 -0500

    Reshape Main.tsx and remove unidiomatic useEffect

---
+++ app/Data.tsx
@@ -0,0 +1,36 @@
+import { DockData } from './action';
+import React from 'react';
+
+import {
+  Box,
+  Container,
+  Paper,
+} from "@mui/material";
+
+export default function Data({
+  dockData,
+  isLoading,
+} : {
+  dockData: DockData|undefined,
+  isLoading: boolean
+}) {
+  if (isLoading) {
+    return (
+      <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%' }}>
+        Loading...
+      </Box>
+    );
+  }
+
+  console.log({ dockData });
+
+  return (
+    <Container>
+      <Paper>
+        <Box>
+          This is where the data goes
+        </Box>
+      </Paper>
+    </Container>
+  );
+}
