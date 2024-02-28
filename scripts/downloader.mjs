254a527af4280a79d3d22503a3f3e08170111012
Zack Youngren <zack@youngren.io>
2024-02-27 23:53:16 -0500

    Update downloader and seeder for new dumb citibike data structure

--- scripts/downloader.mjs
+++ scripts/downloader.mjs
@@ -5,7 +5,7 @@
 
 import { PrismaClient } from '@prisma/client'
 import adm from 'adm-zip';
-import fs from 'fs';
+import concat from 'concat-files';
 import { writeFile } from 'node:fs/promises';
 
 import {
@@ -13,6 +13,8 @@ import {
   ListObjectsV2Command,
   S3Client,
 } from "@aws-sdk/client-s3";
+
+import fs, { readdirSync, rmSync } from 'fs';
 
 const client = new S3Client({ region: 'us-east-1' });
 const prisma = new PrismaClient();
@@ -54,25 +56,47 @@ async function getListOfFileNamesOnS3() {
   return fileNames;
 }
 
-async function getFilesNewerThanNewestData(fileNames) {
-  const { mostRecentMonth, mostRecentYear } = await getMostRecentData();
-
+// As of February 2024 the Citibike team has made the absolutely ludicrous
+// decision to change their naming & file structure scheme. Previous years are
+// rolled up into a single zip file each (e.g. 2023-citibike-tripdata.zip) that
+// contains a single directory (e.g. 2023-citibike-tripdata) which contains
+// monthly directories (e.g. 1_January) which themselves contain multiple zip
+// files for a single month. The current year gets one top level file per month
+// (e.g. 202401-citibike-tripdata.csv.zip) which also contains multiple files
+// for the single month. This is really really dumb and I hate that they did
+// this.
+async function getFilesNewerThanNewestData(fileNames, mostRecentYear, mostRecentMonth) {
   return fileNames.filter(fileName => {
-    // JC- files will safely return NaN and then false
+    // JC- files will return NaN here
     const fileYear = parseInt(fileName.slice(0,4));
+    // Annual files will return NaN here
     const fileMonth = parseInt(fileName.slice(4,6));
 
-    return fileYear > mostRecentYear
-      || (fileYear === mostRecentYear && fileMonth > mostRecentMonth);
+    const fileIsMonthly = Number.isInteger(fileMonth);
+    const fileIsYearly = !fileIsMonthly;
+
+    return (
+      // A whole yearly rollup file of a year ahead of any data we have
+      (fileIsYearly && fileYear > mostRecentYear)
+      // A yearly file for a year that we haven't yet completed
+      || (fileIsYearly && fileYear === mostRecentYear && mostRecentMonth < 12)
+      // A monthly file in a year for which we currently have no data
+      || (fileIsMonthly && fileYear > mostRecentYear)
+      // A monthly file for a year, but not a month, in which we have data
+      || (fileIsMonthly && fileYear === mostRecentYear && fileMonth > mostRecentMonth)
+    );
   });
 }
 
-async function downloadAndUnzipFiles(fileNames) {
+async function downloadAndUnzipFiles(fileNames, mostRecentYear, mostRecentMonth) {
   if (fileNames.length > 0 && !fs.existsSync(TMP_DIR)) {
     fs.mkdirSync(TMP_DIR);
   }
 
-  await fileNames.forEach(async (file) => {
+  // Delete any files left over in this temp dir
+  readdirSync(TMP_DIR).forEach(f => rmSync(`${TMP_DIR}/${f}`));
+
+  for (const file of fileNames) {
     const command = new GetObjectCommand({
       Bucket: BUCKET_NAME,
       Key: file,
@@ -85,18 +109,55 @@ async function downloadAndUnzipFiles(fileNames) {
 
     const zip = new adm(zipLocation);
     const zipEntries = zip.getEntries();
-    zipEntries.forEach(entry => {
-      if (entry.entryName.match(/^\d.*csv$/) !== null) {
-        zip.extractEntryTo(entry.entryName, TMP_DIR, false, true);
+    for (const entry of zipEntries) {
+      // Based on the filtering we did at the point of choosing which files to
+      // download, the only CSVs here that we *don't* want are the ones with
+      // both year and month dated before our most recent data.
+      if (entry.entryName.match(/\.csv$/)) {
+        const fileName = entry.entryName.match(/\d{6}.*\.csv$/)[0];
+        const fileYear = parseInt(fileName.slice(0,4));
+        const fileMonth = parseInt(fileName.slice(4,6));
+
+        if (entry.entryName[0] === '_') {
+          // This is in the __MACOSX subdirectory and we don't want it
+        } else if ((fileYear > mostRecentYear) || (fileMonth > mostRecentMonth)) {
+          await zip.extractEntryTo(entry.entryName, TMP_DIR, false, true);
+        }
       }
-    });
+    }
+
 
     fs.unlinkSync(zipLocation);
-  });
+  }
+}
+
+// Before Citibike migrated to a strictly dumber and worse file scheme, each
+// month was one file. The seeder expects this, and I don't want to rewrite
+// that, so I'm going to concatenate the multiple files per month into one
+// file and tell the seeder to skip the extra header rows.
+async function concatenateFiles() {
+  const files = readdirSync(TMP_DIR);
+  const months = new Set();
+  files.forEach(file => months.add(file.slice(0,6)));
+
+  for (const month of months) {
+    const newFileName = `${TMP_DIR}/${month}.csv`;
+    const thisMonthAbsoluteFiles = files
+      .filter(file => file.indexOf(month) > -1)
+      .map(file => (`${TMP_DIR}/${file}`));
+
+    concat(thisMonthAbsoluteFiles, newFileName, () => {
+      thisMonthAbsoluteFiles.forEach(file => {
+        fs.unlinkSync(file)
+      });
+    });
+  }
 }
 
 (async () => {
+  const { mostRecentMonth, mostRecentYear } = await getMostRecentData();
   const fileNames = await getListOfFileNamesOnS3();
-  const newFileNames = await getFilesNewerThanNewestData(fileNames);
-  await downloadAndUnzipFiles(newFileNames);
+  const newFileNames = await getFilesNewerThanNewestData(fileNames, mostRecentYear, mostRecentMonth);
+  await downloadAndUnzipFiles(newFileNames, mostRecentYear, mostRecentMonth);
+  await concatenateFiles();
 })();
