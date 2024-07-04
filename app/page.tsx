18b13a5f926cee4350676d40b0b3102b5e75383b
Zack Youngren <zack@youngren.io>
2024-07-04 13:34:24 -0400

    FAQ (#10)

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,20 @@
+'use client';
+
+import CancelIcon from '@mui/icons-material/Cancel';
+import FAQ from './FAQ';
+import HelpIcon from '@mui/icons-material/Help';
 import Main from './Main';
 import React from 'react';
+
 import { exoFontFamily } from './ThemeProvider';
+
+import styled from '@emotion/styled';
 
 import { Grid, Typography } from '@mui/material';
 
-export default async function Home() {
+export default function Home() {
+  const [faq, setFaq] = React.useState(false);
+
   return (
     <main>
       <Grid
@@ -23,17 +33,60 @@ export default async function Home() {
             flexDirection: 'column',
           }}
         >
-          <Typography
-            variant='h4'
-            component='h1'
-            sx={{ fontFamily: exoFontFamily, marginTop: 2 }}
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'space-between',
+              marginTop: 2,
+            }}
           >
-            CitiBike Dock Data
-          </Typography>
+            <Typography
+              variant='h4'
+              component='h1'
+              sx={{
+                fontFamily: exoFontFamily,
+                display: 'flex-item',
+              }}
+            >
+              CitiBike Dock Data
+            </Typography>
 
-          <Main />
+            <div style={{ display: 'flex-item' }}>
+              {faq ? (
+                <StyledButton onClick={() => setFaq(false)}>
+                  <CancelIcon /> <Typography> Back </Typography>
+                </StyledButton>
+              ) : (
+                <StyledButton onClick={() => setFaq(true)}>
+                  <HelpIcon /> <Typography> FAQ </Typography>
+                </StyledButton>
+              )}
+            </div>
+          </div>
+
+          {faq && <FAQ />}
+
+          {/* Don't lose state when FAQ is opened */}
+          <div style={{ display: faq ? 'none' : 'block' }}>
+            <Main />
+          </div>
         </Grid>
       </Grid>
     </main>
   );
 }
+
+const StyledButton = styled.button`
+  background: white;
+  border: 0;
+  cursor: pointer;
+  width: 70px;
+
+  &:hover {
+    border-radius: 5px;
+    outline: 3px solid #0150b4;
+    p {
+      font-weight: bold;
+    }
+  }
+`;
