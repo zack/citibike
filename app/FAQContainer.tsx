b4e3f7849946362dcc5a35161743c9428c6350f3
Zack Youngren <zack@youngren.io>
2024-08-12 00:05:54 -0400

    under construction

--- app/FAQContainer.tsx
+++ app/FAQContainer.tsx
@@ -1,11 +1,13 @@
 'use client';
 
-import { Box } from '@mui/material';
 import FAQ from './FAQ';
+import Image from 'next/image';
 import React from 'react';
-import ViewPicker from './ViewPicker';
 
+import { exoFontFamily } from './ThemeProvider';
 import { useQueryState } from 'nuqs';
+
+import { Box, Typography } from '@mui/material';
 
 export default function FAQContainer() {
   const [faqOpen, _] = useQueryState('faq');
@@ -16,7 +18,22 @@ export default function FAQContainer() {
 
       {/* Don't lose state when FAQ is opened */}
       <Box sx={{ display: faqOpen ? 'none' : 'block', height: '100%' }}>
-        <ViewPicker />
+        <Typography
+          component='h1'
+          variant='h4'
+          sx={{ fontFamily: exoFontFamily }}
+        >
+          Fixing the data...
+        </Typography>
+        <Typography>
+          Fixing some stuff! Should be back up in the morning :)
+        </Typography>
+        <Image
+          alt='animated 90s style construction equipment'
+          src='/construction.gif'
+          width={200}
+          height={111}
+        />
       </Box>
     </>
   );
