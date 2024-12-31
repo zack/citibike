e08983dd2838191e8b8103dd948339a863f0fa84
Zack Youngren <zack@youngren.io>
2024-12-30 22:21:41 -0500

    condense WhereSpecifier

--- app/api/timeframe/route.ts
+++ app/api/timeframe/route.ts
@@ -1,7 +1,7 @@
-import { isBorough } from '../../utils';
+import { Timeframe } from '../../types';
+import { getWhereSpecifier } from '../../utils';
 import prisma from '@/prisma/db';
 import { NextRequest, NextResponse } from 'next/server';
-import { Timeframe, WhereSpecifier } from '../../types';
 
 export async function GET(
   request: NextRequest,
@@ -10,28 +10,10 @@ export async function GET(
   const type = searchParams.get('type');
   const specifier = searchParams.get('specifier');
 
-  if (!type || !specifier) {
-    return NextResponse.json(
-      { error: 'Missing type or specifier' },
-      { status: 400 },
-    );
-  }
-
-  let where: WhereSpecifier = {};
-
-  if (type === 'station') {
-    where = { stationId: parseInt(specifier) };
-  } else if (type === 'borough' && isBorough(specifier)) {
-    where = { station: { borough: specifier } };
-  } else if (type === 'community-district') {
-    where = { station: { communityDistrict: parseInt(specifier) } };
-  } else if (type === 'council-district') {
-    where = { station: { councilDistrict: parseInt(specifier) } };
-  } else {
-    return NextResponse.json(
-      { error: 'Invalid type or specifier' },
-      { status: 400 },
-    );
+  const where = getWhereSpecifier(type, specifier);
+  if (where instanceof NextResponse) {
+    // error
+    return where;
   }
 
   const first = await prisma.stationDay.findFirst({
