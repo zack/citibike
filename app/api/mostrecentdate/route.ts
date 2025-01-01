752a3d05eacdf49d65825262bf19250b8e2a813b
Zack Youngren <zack@youngren.io>
2024-12-31 19:06:06 -0500

    Redis (#65)

--- app/api/mostrecentdate/route.ts
+++ app/api/mostrecentdate/route.ts
@@ -1,13 +1,49 @@
 import { NextResponse } from 'next/server';
+import cache from '../../redis';
 import prisma from '@/prisma/db';
 
+function JSONIsValid(json: unknown) {
+  if (typeof json !== 'object' || json === null) {
+    return false;
+  }
+
+  const obj = json as Record<string, number>;
+
+  const keys = Object.keys(obj);
+  if (keys.length !== 2 || !keys.includes('year') || !keys.includes('month')) {
+    return false;
+  }
+
+  return typeof obj.year === 'number' && typeof obj.month === 'number';
+}
+
 export async function GET() {
+  const cacheKey = 'mostrecentdate';
+  const cacheResult = await cache.get(cacheKey);
+
+  if (cacheResult) {
+    try {
+      const cacheJSON = JSON.parse(cacheResult);
+
+      if (JSONIsValid(cacheJSON)) {
+        return NextResponse.json(cacheJSON);
+      } else {
+        cache.del(cacheKey);
+      }
+    } catch {
+      cache.del(cacheKey);
+    }
+  }
+
   const queryResults = await prisma.stationDay.findFirst({
     orderBy: [{ year: 'desc' }, { month: 'desc' }, { day: 'desc' }],
   });
 
-  return NextResponse.json({
+  const json = {
     year: queryResults?.year || 0,
     month: queryResults?.month || 0,
-  });
+  };
+
+  cache.set(cacheKey, JSON.stringify(json));
+  return NextResponse.json(json);
 }
--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -1,14 +1,19 @@
 import type { NextRequest } from 'next/server';
+import cache from '../../redis';
 import { revalidateTag } from 'next/cache';
 import { unauthorized } from 'next/navigation';
 
 export async function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams;
-  const tag = searchParams.get('tag') ?? '';
   const pass = searchParams.get('pass');
 
   if (pass === process.env.PASS) {
-    revalidateTag(tag);
+    await cache.flushAll();
+
+    revalidateTag('stations');
+    revalidateTag('council-districts');
+    revalidateTag('community-districts');
+
     return Response.json({ revalidated: true, now: Date.now() });
   } else {
     unauthorized();
