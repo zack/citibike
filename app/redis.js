184e4f335e7bd98704270769f8cb4d13ed544d32
Zack Youngren <github.com@youngren.io>
2025-05-10 09:10:07 -0400

    ..

--- app/redis.js
+++ app/redis.js
@@ -3,6 +3,8 @@ import { createClient } from 'redis';
 const redisUrl = process.env.REDIS_URL;
 const client = redisUrl ? createClient({ url: redisUrl }) : createClient();
 
+console.log({ redisUrl });
+
 // eslint-disable-next-line
 client.on('error', (err) => console.log('Redis Client Error', err));
 
