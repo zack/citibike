a4946702bd1dc9985cc940021e815faba6c341d6
Zack Youngren <zack@youngren.io>
2025-05-10 10:02:51 -0400

    Add Cypress tests and workflow (#81)
    Add basic Cypress tests and GHA workflow

--- app/redis.js
+++ app/redis.js
@@ -3,9 +3,15 @@ import { createClient } from 'redis';
 const redisUrl = process.env.REDIS_URL;
 const client = redisUrl ? createClient({ url: redisUrl }) : createClient();
 
-// eslint-disable-next-line
-client.on('error', (err) => console.log('Redis Client Error', err));
+// If there's no redisUrl then that means we're in a GitHub workflow, and redis
+// calls will never actually be made since the api calls are mocked out anyway.
+// So we don't need to worry about the fact that we have a nonworking redis client.
 
-await client.connect();
+if (redisUrl) {
+  // eslint-disable-next-line
+  client.on('error', (err) => console.log('Redis Client Error', err));
+
+  await client.connect();
+}
 
 export default client;
---
+++ cypress/e2e/app.cy.ts
@@ -0,0 +1,99 @@
+import boroughChart from '../fixtures/borough/chart.json';
+import boroughTimeframe from '../fixtures/borough/timeframe.json';
+import boroughTopline from '../fixtures/borough/topline.json';
+
+describe('Render', () => {
+  it('should render the home page', () => {
+    cy.visit('http://localhost:3000/');
+
+    cy.get('h1').contains('Citi Bike Station Data');
+  })
+
+  it('should show the FAQ', () => {
+    cy.visit('http://localhost:3000/');
+
+    // open it
+    cy.get('button').contains('FAQ').click();
+
+    cy.get('h2').contains('Frequently Asked Questions');
+
+    // and close it
+    cy.get('button').contains('Back').click();
+
+    cy.contains('h2', 'Frequently Asked Questions').should('not.exist');
+  });
+
+  describe('borough', () => {
+    beforeEach(() => {
+      cy.intercept('GET', '/api/timeframe*', {
+        statusCode: 200,
+        body: boroughTimeframe,
+      }).as('timeframe');
+
+      cy.intercept('GET', '/api/chart*', {
+        statusCode: 200,
+        body: boroughChart,
+      }).as('chart');
+
+      cy.intercept('GET', '/api/topline*', {
+        statusCode: 200,
+        body: boroughTopline,
+      }).as('topline');
+    });
+
+    it('should render borough data', () => {
+      cy.visit('http://localhost:3000/');
+
+      cy.get("button[value='borough']").click();
+      cy.get('#borough-options')
+        .parent()
+        .click()
+        .get("ul > li[data-value='Brooklyn']")
+        .click();
+
+      // nuqs
+      cy.url().should('include', '?view=borough&borough=Brooklyn');
+
+      // topline
+      cy.contains('Stations in Brooklyn have been used 8,729,291 times between May 2020 and April 2025.');
+
+      cy.contains('145,488'); // uses per month
+      cy.contains('4,796'); // uses per day
+      cy.contains('82%'); // on ebikes
+    });
+
+    it('should render the advanced data', () => {
+      cy.visit('http://localhost:3000/');
+
+      cy.get("button[value='borough']").click();
+      cy.get('#borough-options')
+        .parent()
+        .click()
+        .get("ul > li[data-value='Brooklyn']")
+        .click();
+
+
+      // expand the advanced data
+      cy.get('button').contains('Show more data').click();
+
+      // test that the chart at least rendered
+      cy.get('.recharts-cartesian-grid'); // not sure how to test better than this
+
+      // click over to the table
+      cy.get('button').contains('Table').click();
+
+      // just one of the cells
+      cy.contains('24,529');
+    });
+
+    describe('query params', () => {
+      it('should work with params in the url', () => {
+        cy.visit('https://www.citibikedata.nyc/?view=borough&borough=Brooklyn');
+
+        cy.contains('145,488'); // uses per month
+        cy.contains('4,796'); // uses per day
+        cy.contains('82%'); // on ebikes
+      });
+    });
+  });
+});
